36768c093429506b6679420f9f3d2d01e358987f
Steven Chidley <schidley@gmail.com>
2025-01-21 10:27:19 +0000

    database linking fixed

---
+++ buttonclicks/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Clicks
+
+admin.site.register(Clicks)
--- buttonclicks/settings.py
+++ buttonclicks/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gqu3%x#1#cbd5vvqxa8mv7h@wc$2z^hhvv5sjfd6hw+fhrey_y'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
