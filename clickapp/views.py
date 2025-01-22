bc67184bde84b35cd21532846fc9c3826d4764bd
Steven Chidley <schidley@gmail.com>
2025-01-22 06:16:54 +0000

    placeholder message replaced with placeholder page

--- clickapp/views.py
+++ clickapp/views.py
@@ -5,4 +5,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. Welcome to the Button Clicking Project!")
+    return render(request, 'index.html')
+    
+
