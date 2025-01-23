d886bbb89e587c6b7275e576a415667b82588d2a
Steven Chidley <schidley@gmail.com>
2025-01-23 06:07:43 +0000

    count increment linked into database

--- clickapp/urls.py
+++ clickapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('', views.index, name='index'),
+    path('increment_click_count/', views.increment_click_count, name='increment_click_count'), 
 ]
--- clickapp/views.py
+++ clickapp/views.py
@@ -3,13 +3,19 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from .models import Click
 
 
 # Create your views here.
 
+
 @login_required
 def index(request):
-    return render(request, 'index.html')
+    try:
+        click = Click.objects.get(user=request.user)
+    except Click.DoesNotExist:
+        click = Click.objects.create(user=request.user)
+    return render(request, 'index.html', {'click_count': click.count})
 
 def login_view(request):
     if request.method == 'POST':
@@ -27,9 +33,25 @@ def login_view(request):
 
 
 
-
 @login_required
-def index(request):
-    return render(request, 'index.html')
+def increment_click_count(request):
+    if request.method == 'POST':
+        click, created = Click.objects.get_or_create(user=request.user)
+        click.count += 1
+        click.save()
+        return JsonResponse({'click_count': click.count})
+    return JsonResponse({'error': 'Invalid request'}, status=400)
 
-
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
