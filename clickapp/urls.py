44380cb5fb741f95630754bcc1864fcd48383689
Steven Chidley <schidley@gmail.com>
2025-01-27 00:25:12 +0000

    logout view added

--- clickapp/urls.py
+++ clickapp/urls.py
@@ -1,6 +1,7 @@
 # myapp/urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('increment_click_count/', views.increment_click_count, name='increment_click_count'), 
     path('leaderboard/', views.leaderboard, name='leaderboard'),
     path('update_btext/', views.update_btext, name='update_btext'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
